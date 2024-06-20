f0a45b8a3a68f5477c79d450bc255e0d546f0ca8
Erik Koppes <koppesea@MWRMANN0001MAC.local>
2024-06-20 19:34:02 -0400

    nfcore-rerun

--- Sscr_PWS_RNA-Seq/RNAvar_nfcore/VCF_INFOremovespaces.sh
+++ Sscr_PWS_RNA-Seq/RNAvar_nfcore/VCF_INFOremovespaces.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t 1-00:00 # Runtime in D-HH:MM
 #SBATCH -J VCFedit
-#SBATCH --output=htseq_PWSSscrofav98-%A_%a.out
+#SBATCH --output=htslib_VCFnoINFOwhitespace-%A_%a.out
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16g #
 
@@ -11,4 +11,4 @@ module load gcc/8.2.0
 module load htslib/1.9
 
 bgzip -d -c sus_scrofa.vcf.gz | awk -v FS='\t' -v OFS='\t' '{gsub(/ /, "_", $8); print}' | bgzip -c >  sus_scrofa_forGATK.vcf.gz
-tabix -p sus_scrofa_forGATK.vcf.gz
+tabix -p vcf sus_scrofa_forGATK.vcf.gz
